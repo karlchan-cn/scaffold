5ba519b5cd7eb939bb92ede8c822d49282c477d8
80166776 <chenjinlong@oppo.com>
2021-05-24 15:44:50 +0800

    add:调整部分打包方式

---
+++ core/src/test/java/com/scaffold/core/hessian/HessianRunner.java
@@ -0,0 +1,13 @@
+package com.scaffold.core.hessian;
+
+import com.caucho.hessian.io.HessianOutput;
+
+import java.io.ByteArrayOutputStream;
+
+public class HessianRunner {
+    public static void main(String[] args) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+       // Hessian2Output out = new Hessian2Output(bos);
+    }
+}
--- quick-test/src/main/java/com/scaffold/http/get/HttpGetRunner.java
+++ quick-test/src/main/java/com/scaffold/http/get/HttpGetRunner.java
@@ -1,18 +1,114 @@
 package com.scaffold.http.get;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * http get方法测试类
  */
 public class HttpGetRunner {
-    public static void main(String[] args) {
-
-    }
 
     private static final void testGetMethodUrlLimitation() {
         //HttpClient client
     }
 
+    /**
+     * 发送get请求,  下载图片
+     *
+     * @param client   httpclient
+     * @param savePath 保存地址
+     * @param imgUrl   图片http路径
+     */
+    public static void httpGetImg(CloseableHttpClient client, String imgUrl, String savePath) {
+
+
+        // 发送get请求
+        HttpGet request = new HttpGet(imgUrl);
+        // 设置请求和传输超时时间
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(50000).setConnectTimeout(50000).build();
+
+        //设置请求头
+        request.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.79 Safari/537.1");
+
+        request.setConfig(requestConfig);
+        try {
+            CloseableHttpResponse response = client.execute(request);
+
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                HttpEntity entity = response.getEntity();
+
+                InputStream in = entity.getContent();
+
+                FileUtils.copyInputStreamToFile(in, new File(savePath));
+                System.out.println("下载图片成功:" + imgUrl);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            request.releaseConnection();
+
+        }
+    }
+
+    /**
+     * 组装图片地址
+     *
+     * @param prefix      前缀
+     * @param index       当前图片索引
+     * @param indexLength 图片索引长度
+     * @return 组装后的图片地址
+     */
+    private static String constructImgUrl(String prefix, int index, int indexLength) {
+        String indexStr = String.valueOf(index);
+        for (int i = indexStr.length(); i < indexLength; i++) {
+            indexStr = "0" + indexStr;
+        }
+        return prefix + indexStr + ".jpg";
+    }
+
+    public static void main(String[] args) {
+
+        CloseableHttpClient client = null;
+
+        try {
+            client = HttpClients.createDefault();
+            String prefix = "https://static001.geekbang.org/con/30/pdf/2044952145/image/page-";
+            for (int i = 1; i <= 38; i++) {
+                String path = "E:\\dev\\ArchSubmit\\archsubmmit-images-container\\百度智能流量监控实战AIOps";
+                httpGetImg(client, constructImgUrl(prefix, i, 3), path + "/" + i + ".jpg");
+                System.out.println("ok");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
+
+
+
 
